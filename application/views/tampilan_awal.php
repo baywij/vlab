d7d339fe9e43bd364a62efaf17e3d7c840ac81b6
Bayu WIjaya <bayu.wijaya06@gmail.com>
2018-04-20 23:11:24 +0700

    haha

--- application/views/tampilan_awal.php
+++ application/views/tampilan_awal.php
@@ -7,11 +7,7 @@
 <link rel="shortcut icon" href="<?php echo base_url();?>assets/images/logo.png">
 <!-- Set the viewport width to device width for mobile -->
 <meta name="viewport" content="width=device-width"/>
-<<<<<<< HEAD
 <title>VIRTUAL LAB</title>
-=======
-<title>Manajemen Keuangan 2 - E-learning Tes Git</title>
->>>>>>> bayu
 <!-- CSS Files-->
 <!-- bootstrap & fontawesome -->
 <link rel="stylesheet" href="<?php echo base_url();?>assets/css/bootstrap.min.css" />
